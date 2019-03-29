8bc9ea11f68124681c53f3f5506ba0c74415a904
Yesterday17 <t@yesterday17.cn>
2019-03-30 02:14:31 +0800

    Most features.

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,16 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:path_provider/path_provider.dart';
-
-FlutterLocalNotificationsPlugin notification;
+import 'package:permission_handler/permission_handler.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   notification = FlutterLocalNotificationsPlugin();
   runApp(MyApp());
 }
+
+FlutterLocalNotificationsPlugin notification;
 
 class MyApp extends StatelessWidget {
   @override
@@ -26,9 +27,9 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
   final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -36,10 +37,115 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   bool isQQ = true;
+  String picture = '';
+
+  Future<String> get _localPath async {
+    final directory = await getExternalStorageDirectory();
+    return directory.path +
+        '/tencent/' +
+        (isQQ ? 'MobileQQ' : 'Tim') +
+        '/diskcache';
+  }
+
+  Future<String> get _toPath async {
+    final directory = await getExternalStorageDirectory();
+    return directory.path + '/FlashPicture/';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('点击右下角按钮打开通知'),
+            Text('在想要保存闪照时点击通知即可'),
+            FlatButton(
+              child: Text('切换为 ' + (isQQ ? 'TIM' : 'QQ')),
+              onPressed: _toggleQQTim,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showNotification,
+        tooltip: '显示通知',
+        child: Icon(Icons.play_arrow),
+      ),
+    );
+  }
+
+  Future<bool> checkPermission() async {
+    var status = await PermissionHandler()
+        .checkPermissionStatus(PermissionGroup.storage);
+    return status == PermissionStatus.granted;
+  }
+
+  Future copyFlashPicture(String path) async {
+    var dir = Directory(await _toPath);
+    if (!dir.existsSync()) {
+      dir.createSync();
+    }
+
+    var result = (await _toPath) + picture + '.png';
+    File(path).copySync(result);
+    // showToast('已保存至: ' + result);
+  }
+
+  Future getFlashPicture(String payload) async {
+    // 1. Check whether the cache folder exists
+    var exists = await _localPathExists();
+    if (!exists) {
+      showToast('缓存目录不存在！');
+      return;
+    }
+
+    // 2. Get files in cache folder
+    List<FileSystemEntity> list;
+    try {
+      list = Directory(await _localPath)
+          .listSync(recursive: true)
+          .where((item) => item.uri.pathSegments.last.endsWith('fp'))
+          .toList();
+    } catch (e) {
+      debugPrint(e.toString());
+      return;
+    }
+    if (list.length > 0) {
+      list.sort((a, b) => (a.statSync().modified.millisecondsSinceEpoch <
+              b.statSync().modified.millisecondsSinceEpoch)
+          ? 1
+          : -1);
+
+      for (var item in list) {
+        // showToast('发现闪照: ' + item.uri.pathSegments.last);
+        // showToast('修改时间: ' + item.statSync().modified.toString());
+        picture = item.uri.pathSegments.last;
+        await copyFlashPicture(item.path);
+        // break;
+      }
+    }
+
+    // FlashPicture Not Found
+    if (picture == '') {
+      showToast('未找到闪照！');
+    }
+    picture = ''; // Reset picture
+  }
 
   @override
   void initState() {
     super.initState();
+
+    if (!Platform.isAndroid) {
+      showToast('只支持 Android 平台！');
+      exit(0);
+    }
+
     var initializationSettingsAndroid =
         new AndroidInitializationSettings('app_icon');
     var initializationSettingsIOS = new IOSInitializationSettings();
@@ -55,98 +161,42 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  Future<bool> checkPermission() async {
-    var status = await PermissionHandler()
-        .checkPermissionStatus(PermissionGroup.storage);
-    return status == PermissionStatus.granted;
-  }
-
   requestPermission() async {
-    await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+    Map<PermissionGroup, PermissionStatus> status =
+        await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+    if (status[PermissionGroup.storage] != PermissionStatus.granted) {
+      showToast('存储权限必须！');
+      exit(0);
+    }
   }
 
-  void _toggleQQTim() {
-    setState(() {
-      isQQ = !isQQ;
-    });
-  }
-
-  Future<String> get _localPath async {
-    final directory = await getExternalStorageDirectory();
-    return directory.path +
-        '/tencent/' +
-        (isQQ ? 'MobileQQ' : 'Tim') +
-        '/diskcache';
+  void showToast(String message) {
+    Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIos: 3,
+        backgroundColor: Colors.blueGrey,
+        textColor: Colors.white,
+        fontSize: 12.0);
   }
 
   Future<bool> _localPathExists() async => Directory(await _localPath).exists();
 
-  void _showNotification() async {
-    var exists = await _localPathExists();
-    debugPrint(await _localPath);
-    if (!exists) {
-      debugPrint('Not Exist!');
-      return;
-    }
-
+  void _showNotification() {
     var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
         'fpc', 'FlashPicture Capturer', 'Capture flash picture of QQ or TIM.',
         importance: Importance.Max, priority: Priority.Max, autoCancel: false);
     var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
     var platformChannelSpecifics = new NotificationDetails(
         androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-    notification.show(
-        0, "QQ/TIM 闪照获取工具", "当需要保存闪照时，点击该通知。", platformChannelSpecifics);
-    _localPath;
+    notification.show(0, (isQQ ? 'QQ' : 'TIM') + ' 闪照获取工具', '当需要保存闪照时，点击该通知。',
+        platformChannelSpecifics);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '目前只支持QQ',
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _showNotification,
-        tooltip: '显示通知',
-        child: Icon(Icons.play_arrow),
-      ),
-    );
-  }
-
-  Future getFlashPicture(String payload) async {
-    await requestPermission();
-    var path = await _localPath;
-    List<FileSystemEntity> list;
-    try {
-      list = Directory(path).listSync(recursive: true);
-    } catch (e) {
-      debugPrint(e.toString());
-      return;
-    }
-    if (list.length > 0) {
-      list.sort((a, b) => (a.statSync().modified.millisecondsSinceEpoch <
-              b.statSync().modified.millisecondsSinceEpoch)
-          ? 1
-          : -1);
-
-      for (var item in list) {
-        if (item.uri.pathSegments.last.endsWith("_fp")) {
-          debugPrint('发现闪照: ' + item.path);
-          debugPrint('修改时间: ' + item.statSync().modified.toString());
-          break;
-        }
-      }
-    }
+  void _toggleQQTim() {
+    setState(() {
+      isQQ = !isQQ;
+    });
   }
 }
