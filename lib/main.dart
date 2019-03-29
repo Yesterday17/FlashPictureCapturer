51d623612035ca4ebd421dbc11a1d654009164c4
Yesterday17 <t@yesterday17.cn>
2019-03-30 01:00:32 +0800

    Find image file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,32 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:path_provider/path_provider.dart';
+
+FlutterLocalNotificationsPlugin notification;
+
+void main() {
+  notification = FlutterLocalNotificationsPlugin();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'QQ/TIM 闪照获取工具',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'QQ/TIM 闪照获取工具'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +35,118 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  bool isQQ = true;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    var initializationSettingsAndroid =
+        new AndroidInitializationSettings('app_icon');
+    var initializationSettingsIOS = new IOSInitializationSettings();
+    var initializationSettings = new InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+    notification.initialize(initializationSettings,
+        onSelectNotification: getFlashPicture);
+
+    checkPermission().then((valid) {
+      if (!valid) {
+        requestPermission();
+      }
     });
+  }
+
+  Future<bool> checkPermission() async {
+    var status = await PermissionHandler()
+        .checkPermissionStatus(PermissionGroup.storage);
+    return status == PermissionStatus.granted;
+  }
+
+  requestPermission() async {
+    await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+  }
+
+  void _toggleQQTim() {
+    setState(() {
+      isQQ = !isQQ;
+    });
+  }
+
+  Future<String> get _localPath async {
+    final directory = await getExternalStorageDirectory();
+    return directory.path +
+        '/tencent/' +
+        (isQQ ? 'MobileQQ' : 'Tim') +
+        '/diskcache';
+  }
+
+  Future<bool> _localPathExists() async => Directory(await _localPath).exists();
+
+  void _showNotification() async {
+    var exists = await _localPathExists();
+    debugPrint(await _localPath);
+    if (!exists) {
+      debugPrint('Not Exist!');
+      return;
+    }
+
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+        'fpc', 'FlashPicture Capturer', 'Capture flash picture of QQ or TIM.',
+        importance: Importance.Max, priority: Priority.Max, autoCancel: false);
+    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+    var platformChannelSpecifics = new NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    notification.show(
+        0, "QQ/TIM 闪照获取工具", "当需要保存闪照时，点击该通知。", platformChannelSpecifics);
+    _localPath;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              '目前只支持QQ',
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _showNotification,
+        tooltip: '显示通知',
+        child: Icon(Icons.play_arrow),
+      ),
     );
+  }
+
+  Future getFlashPicture(String payload) async {
+    await requestPermission();
+    var path = await _localPath;
+    List<FileSystemEntity> list;
+    try {
+      list = Directory(path).listSync(recursive: true);
+    } catch (e) {
+      debugPrint(e.toString());
+      return;
+    }
+    if (list.length > 0) {
+      list.sort((a, b) => (a.statSync().modified.millisecondsSinceEpoch <
+              b.statSync().modified.millisecondsSinceEpoch)
+          ? 1
+          : -1);
+
+      for (var item in list) {
+        if (item.uri.pathSegments.last.endsWith("_fp")) {
+          debugPrint('发现闪照: ' + item.path);
+          debugPrint('修改时间: ' + item.statSync().modified.toString());
+          break;
+        }
+      }
+    }
   }
 }
